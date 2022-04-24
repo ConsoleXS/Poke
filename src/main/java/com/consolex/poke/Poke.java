a31f3e7dd2a7a72f3b3da1eb41311325c92c40d8
ConsoleXS <aditya.rao.2019.yeet@gmail.com>
2022-04-23 23:05:00 -0500

    Added poke preferences

--- src/main/java/com/consolex/poke/Poke.java
+++ src/main/java/com/consolex/poke/Poke.java
@@ -1,12 +1,15 @@
 package com.consolex.poke;
 
+import com.consolex.poke.commands.PokeCommand;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -27,10 +30,16 @@ public final class Poke extends JavaPlugin implements Listener {
         getServer().getConsoleSender().sendMessage(ChatColor.YELLOW + "Poke Enabled! " + ChatColor.AQUA + "@1.0");
         getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "----------");
         getServer().getPluginManager().registerEvents(this, this);
+        getCommand("poke").setExecutor(new PokeCommand());
 
         config.addDefault("poke-cooldown", 3);
         config.options().copyDefaults(true);
         saveConfig();
+
+        for (Player p : Bukkit.getServer().getOnlinePlayers())
+        {
+            pokePreference.put(p, Sound.ENTITY_EXPERIENCE_ORB_PICKUP);
+        }
     }
 
     @Override
@@ -83,6 +92,36 @@ public final class Poke extends JavaPlugin implements Listener {
         event.setMessage(msg);
     }
 
+    @EventHandler
+    public void setPokePreference(InventoryClickEvent event) {
+        Player player = (Player) event.getWhoClicked();
 
-
+        if (event.getView().getTitle().equalsIgnoreCase("Poke Sound Preference")) {
+            player.playSound(player.getLocation(), Sound.UI_BUTTON_CLICK, 1, 1);
+            switch (event.getCurrentItem().getType()) {
+                case AMETHYST_SHARD:
+                    pokePreference.replace(player, Sound.BLOCK_AMETHYST_BLOCK_BREAK);
+                    break;
+                case RESPAWN_ANCHOR:
+                    pokePreference.replace(player, Sound.BLOCK_RESPAWN_ANCHOR_CHARGE);
+                    break;
+                case BELL:
+                    pokePreference.replace(player, Sound.BLOCK_BELL_USE);
+                    break;
+                case SLIME_BLOCK:
+                    pokePreference.replace(player, Sound.ENTITY_ELDER_GUARDIAN_FLOP);
+                    break;
+                case NOTE_BLOCK:
+                    pokePreference.replace(player, Sound.BLOCK_NOTE_BLOCK_PLING);
+                    break;
+                case GRAY_STAINED_GLASS:
+                    pokePreference.replace(player, Sound.ENTITY_EXPERIENCE_ORB_PICKUP);
+                    break;
+            }
+            player.playSound(player.getLocation(), pokePreference.get(player), 1, 1);
+            player.sendMessage(ChatColor.AQUA + "New sound set!");
+            player.closeInventory();
+            event.setCancelled(true);
+        }
+    }
 }
---
+++ src/main/java/com/consolex/poke/commands/PokeCommand.java
@@ -0,0 +1,65 @@
+package com.consolex.poke.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+
+public class PokeCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player)
+        {
+            Player player = (Player) sender;
+            Inventory gui = Bukkit.createInventory(player, 9, "Poke Sound Preference");
+
+
+            ItemStack amethyst = new ItemStack(Material.AMETHYST_SHARD, 1);
+            ItemMeta amethyst_meta = amethyst.getItemMeta();
+            amethyst_meta.setDisplayName(ChatColor.WHITE + "Ping: Amethyst block break");
+            amethyst.setItemMeta(amethyst_meta);
+
+            ItemStack respawnAnchor = new ItemStack(Material.RESPAWN_ANCHOR, 1);
+            ItemMeta respawnAnchor_meta = respawnAnchor.getItemMeta();
+            respawnAnchor_meta.setDisplayName(ChatColor.WHITE + "Ping: Respawn Anchor Fill");
+            respawnAnchor.setItemMeta(respawnAnchor_meta);
+
+            ItemStack bell = new ItemStack(Material.BELL, 1);
+            ItemMeta bell_meta = bell.getItemMeta();
+            bell_meta.setDisplayName(ChatColor.WHITE + "Ping: Bell hit");
+            bell.setItemMeta(bell_meta);
+
+            ItemStack slime = new ItemStack(Material.SLIME_BLOCK, 1);
+            ItemMeta slime_meta = slime.getItemMeta();
+            slime_meta.setDisplayName(ChatColor.WHITE + "Ping: Slimy sound");
+            slime.setItemMeta(slime_meta);
+
+
+            ItemStack pling = new ItemStack(Material.NOTE_BLOCK, 1);
+            ItemMeta pling_meta = pling.getItemMeta();
+            pling_meta.setDisplayName(ChatColor.WHITE + "Ping: Pling noteblock sound");
+            pling.setItemMeta(pling_meta);
+
+
+            ItemStack empty = new ItemStack(Material.GRAY_STAINED_GLASS_PANE, 1);
+            ItemMeta empty_meta = empty.getItemMeta();
+            empty_meta.setDisplayName(ChatColor.RED + "Reset " + ChatColor.WHITE + "sound to default");
+            empty.setItemMeta(empty_meta);
+
+            ItemStack[] soundPreferenceItems = {empty, empty, amethyst, respawnAnchor, bell, slime, pling, empty, empty};
+            gui.setContents(soundPreferenceItems);
+            player.openInventory(gui);
+
+        }
+
+        return true;
+    }
+}
